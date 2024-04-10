69ae25632e90665caa33a970600137e95726d9e3
rafxbc <rafaelabebercoelho@gmail.com>
2024-04-10 18:22:13 -0300

    feat: criação do Util.java e application

---
+++ projeto_individual/src/br/com/poo/individual/application/IndividualApplication.java
@@ -0,0 +1,11 @@
+package br.com.poo.individual.application;
+
+import java.io.IOException;
+import br.com.poo.individual.io.RelatorioIO;
+
+public class IndividualApplication {
+	public static void main (String[] args) throws IOException {
+		
+		RelatorioIO.leitor("banco");
+	}
+}
