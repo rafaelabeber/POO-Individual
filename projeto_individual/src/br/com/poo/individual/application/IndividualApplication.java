7bae00cbb581d90095c0e167ff3aedfe8d3c03a4
Rafaela Beber Coelho <162373363+rafxbc@users.noreply.github.com>
2024-04-17 20:17:10 -0300

    fix: inserção do submenuservice no application

--- projeto_individual/src/br/com/poo/individual/application/IndividualApplication.java
+++ projeto_individual/src/br/com/poo/individual/application/IndividualApplication.java
@@ -2,10 +2,12 @@ package br.com.poo.individual.application;
 
 import java.io.IOException;
 import br.com.poo.individual.io.RelatorioIO;
+import br.com.poo.individual.services.SubMenuService;
 
 public class IndividualApplication {
 	public static void main (String[] args) throws IOException {
 		
 		RelatorioIO.leitor("banco");
+		SubMenuService.menu();
 	}
 }
