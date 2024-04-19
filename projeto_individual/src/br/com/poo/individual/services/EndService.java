a17d589b8b5e78aa80ab0c021a27a4806df54031
Rafaela Beber Coelho <162373363+rafxbc@users.noreply.github.com>
2024-04-18 22:41:51 -0300

    fix: end service mudança nos métodos

--- projeto_individual/src/br/com/poo/individual/services/EndService.java
+++ projeto_individual/src/br/com/poo/individual/services/EndService.java
@@ -4,29 +4,33 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.com.poo.individual.entities.Delegacia;
 import br.com.poo.individual.entities.Endereco;
 import br.com.poo.individual.io.RelatorioIO;
 
 public class EndService {
 
-
-	public List<String> listaBairros() throws IOException {
+	public void listaBairros() throws IOException {
 		List<String> listaBairros = new ArrayList<>();
 		for (Endereco endereco : Endereco.getMapaEndereco().values()) {
 			listaBairros.add(endereco.getBairro());
 		}
 		RelatorioIO.relatorioListaBairros(listaBairros);
-		return listaBairros;
 	}
 
-	public static List<Endereco> listaEnderecos;
-
-	public void listaEndereco() throws IOException {
+	public List<Endereco> listaEnderecos() {
 		List<Endereco> listaEndereco = new ArrayList<>();
 
 		for (Endereco endereco : Endereco.getMapaEndereco().values()) {
 			listaEndereco.add(endereco);
 		}
+		return listaEndereco;
+	}
+
+	public List<String> listaBairrosTela() throws IOException {
+		List<String> listaBairros = new ArrayList<>();
+		for (Endereco endereco : Endereco.getMapaEndereco().values()) {
+			listaBairros.add(endereco.getBairro());
+		}
+		return listaBairros;
 	}
 }
