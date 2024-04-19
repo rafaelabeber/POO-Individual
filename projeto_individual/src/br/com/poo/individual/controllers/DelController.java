e900ae96be0cce9785c6f084828ef0b4c804d965
Rafaela Beber Coelho <162373363+rafxbc@users.noreply.github.com>
2024-04-18 22:39:01 -0300

    fix: del controller método lista delegacias e lista nomes delegacias

--- projeto_individual/src/br/com/poo/individual/controllers/DelController.java
+++ projeto_individual/src/br/com/poo/individual/controllers/DelController.java
@@ -5,26 +5,30 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import br.com.poo.individual.entities.Delegacia;
 import br.com.poo.individual.services.DelService;
 import br.com.poo.individual.util.Util;
 
 public class DelController {
 
-	
 	static Logger logger = Util.setupLogger();
 
-	public void listaDelegacias() throws IOException {
-		List<String> delegacias = DelService.listaDelegacias();
+	//console
+	public void listaNomesDelegacias() throws IOException {
+		List<String> delegacias = DelService.listaNomesDelegaciasTela();
 		Util.customizer();
 		logger.log(Level.INFO, () -> delegacias + "\n\nLista gerada com sucesso!");
 	}
-	
+	//impresso
+	public void listaDelegacias() throws IOException {
+		DelService.listaDelegacias();
+		Util.customizer();
+		logger.log(Level.INFO, () -> "\n\nLista gerada com sucesso!");
+	}
+	//impresso
 	public void listaDelegaciaEndereco() throws IOException {
-		DelService delService = new DelService();
-		delService.listaDelegaciaEndereco();
+		DelService.listaDelegaciaEndereco();
 		Util.customizer();
 		logger.log(Level.INFO, "\n\nLista gerada com sucesso!");
 	}
-	
+
 }
