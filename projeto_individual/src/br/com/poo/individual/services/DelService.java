89d285ae75e9c45639fb09546e9fa358528124ba
Rafaela Beber Coelho <162373363+rafxbc@users.noreply.github.com>
2024-04-18 22:41:36 -0300

    fix: del service mudança nos métodos

--- projeto_individual/src/br/com/poo/individual/services/DelService.java
+++ projeto_individual/src/br/com/poo/individual/services/DelService.java
@@ -5,21 +5,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.com.poo.individual.entities.Delegacia;
-import br.com.poo.individual.entities.Endereco;
 import br.com.poo.individual.io.RelatorioIO;
 
 public class DelService {
 
-	public static List<String> listaDelegacias() {
-		List<String> listaDelegacias = new ArrayList<>();
+	public static List<String> listaNomesDelegaciasTela() {
+		List<String> listaNomesDelegacias = new ArrayList<>();
 		for (Delegacia delegacia : Delegacia.getMapaDelegacia().values()) {
-			listaDelegacias.add(delegacia.getNome());
+			listaNomesDelegacias.add(delegacia.getNome());
+		}
+		return listaNomesDelegacias;
+
+	}
+
+	public static void listaDelegaciaEndereco() throws IOException {
+		EndService endService = new EndService();
+		RelatorioIO.relatorioDelegaciaEndereco(obterDelegacias(), endService.listaEnderecos());
+	}
+
+	public static List<Delegacia> listaDelegacias() throws IOException {
+		List<Delegacia> listaDelegacias = new ArrayList<>();
+		for (Delegacia delegacia : Delegacia.getMapaDelegacia().values()) {
+			listaDelegacias.add(delegacia);
+		}
+		RelatorioIO.relatorioListaDelegacias(listaDelegacias);
+		return listaDelegacias;
+
+	}
+
+	public static List<Delegacia> obterDelegacias() throws IOException {
+		List<Delegacia> listaDelegacias = new ArrayList<>();
+		for (Delegacia delegacia : Delegacia.getMapaDelegacia().values()) {
+			listaDelegacias.add(delegacia);
 		}
 		return listaDelegacias;
-	
-	}
-	public void listaDelegaciaEndereco() throws IOException {
-		EndService endService = new EndService();
-		RelatorioIO.relatorioDelegaciaEndereco(endService.listaEndereco(), listaDelegacias());
+
 	}
 }
